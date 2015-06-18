2f1de8776f9ee6bf7e04f9e3afd8455161569be9
C-o-r-E <can.of.tuna@gmail.com>
2015-06-18 15:24:18 -0400

    invert extruder stepper dir

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -375,10 +375,10 @@ const bool Z_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the logic 
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
-#define INVERT_E1_DIR false
-#define INVERT_E2_DIR false
-#define INVERT_E3_DIR false
+#define INVERT_E0_DIR true
+#define INVERT_E1_DIR true
+#define INVERT_E2_DIR true
+#define INVERT_E3_DIR true
 
 // @section homing
 
